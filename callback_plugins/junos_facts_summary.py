66fae90406de7ec7212e87c1418c1a7413e5e2e4
m83880 <jsin@tdc.dk>
2018-04-25 15:02:06 +0200

    Testing the facts plugin

--- callback_plugins/junos_facts_summary.py
+++ callback_plugins/junos_facts_summary.py
@@ -34,29 +34,28 @@ class CallbackModule(CallbackBase):
     #self._display.display(str(result._result))
     if 'invocation' in result._result:
       module_args = result._result['invocation']['module_args']
-    if 'ansible_facts' not in module_args:
-        return None
+    if 'ansible_facts' not in result._result:
+        return
     ansible_facts = result._result['ansible_facts']
     if 'junos' not in ansible_facts:
         return
     junos_dict = ansible_facts['junos']
-
     if 'has_2RE' not in junos_dict:
         return
     junos_has_2RE = junos_dict['has_2RE']
     if junos_has_2RE:
-        self._display.display('Device has 2 Routing engines. Please find details below:', color='blue')
+        self._display.display('Device has 2 Routing engines. Please find details below:', color='violet')
     else:
-        self._display.display('Device has only 1 Routing engine. Please find details below:', color='blue')
+        self._display.display('Device has only 1 Routing engine. Please find details below:', color='violet')
     self._display.display('\tSERIAL NUMBER: {}'.format(junos_dict['serialnumber']), color='blue')   
     self._display.display('\tMASTER RE: {}'.format(junos_dict['master']), color='blue')
-    self._display.display('\tModel: {}'.format(junos_dict['model']), color='blue')
+    self._display.display('\tMODEL: {}'.format(junos_dict['model']), color='blue')
     self._display.display('\tSWITCH STYLE: {}'.format(junos_dict['switch_style']), color='blue')
     if 'RE0' in junos_dict:
-        self._display.display('\tRouting Engine RE0:', color='blue')
+        self._display.display('\tRouting Engine RE0:', color='violet')
         for key, val in junos_dict['RE0'].items():
             self._display.display('\t\t{0}: {1}'.format(key.upper().replace('_', ' '), val), color='blue')    
     if 'RE1' in junos_dict and junos_has_2RE:
-        self._display.display('\tRouting Engine RE1:', color='blue')
+        self._display.display('\tRouting Engine RE1:', color='violet')
         for key, val in junos_dict['RE1'].items():
             self._display.display('\t\t{0}: {1}'.format(key.upper().replace('_', ' '), val), color='blue')
