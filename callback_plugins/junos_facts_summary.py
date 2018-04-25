952d56fd7fea3dd88da1357b24121ed10a843dd1
m83880 <jsin@tdc.dk>
2018-04-25 14:42:15 +0200

    Testing the facts plugin

--- callback_plugins/junos_facts_summary.py
+++ callback_plugins/junos_facts_summary.py
@@ -57,20 +57,20 @@ class CallbackModule(CallbackBase):
     junos_has_2RE = junos_dict['has_2RE']
     
     if junos_has_2RE:
-        self._display.display('Device has 2 Routing engines. Please find details below:')
+        self._display.display('Device has 2 Routing engines. Please find details below:', color='blue')
     else:
-        self._display.display('Device has only 1 Routing engine. Please find details below:')
+        self._display.display('Device has only 1 Routing engine. Please find details below:', color='blue')
     
-    self._display.display('\tSERIAL NUMBER: {}'.format(junos_dict['serialnumber']))   
-    self._display.display('\tMASTER RE: {}'.format(junos_dict['master']))
-    self._display.display('\tModel: {}'.format(junos_dict['model']))
-    self._display.display('\tSWITCH STYLE: {}'.format(junos_dict['switch_style']))
+    self._display.display('\tSERIAL NUMBER: {}'.format(junos_dict['serialnumber']), color='blue')   
+    self._display.display('\tMASTER RE: {}'.format(junos_dict['master']), color='blue')
+    self._display.display('\tModel: {}'.format(junos_dict['model']), color='blue')
+    self._display.display('\tSWITCH STYLE: {}'.format(junos_dict['switch_style']), color='blue')
     
     if 'RE0' in junos_dict:
-        self._display.display('\tRouting Engine RE0:')
+        self._display.display('\tRouting Engine RE0:', color='blue')
         for key, val in junos_dict['RE0'].items():
-            self._display.display('\t\t{0}: {1}'.format(key.upper().replace('_', ' '), val))    
+            self._display.display('\t\t{0}: {1}'.format(key.upper().replace('_', ' '), val), color='blue')    
     if 'RE1' in junos_dict and junos_has_2RE:
-        self._display.display('\tRouting Engine RE1:')
+        self._display.display('\tRouting Engine RE1:', color='blue')
         for key, val in junos_dict['RE1'].items():
-            self._display.display('\t\t{0}: {1}'.format(key.upper().replace('_', ' '), val))
+            self._display.display('\t\t{0}: {1}'.format(key.upper().replace('_', ' '), val), color='blue')
