a140ff6d22a484d9b78f38b6d3102363bcb8ab8d
m83880 <jsin@tdc.dk>
2018-04-25 14:28:34 +0200

    Testing the facts plugin

--- callback_plugins/junos_facts_summary.py
+++ callback_plugins/junos_facts_summary.py
@@ -57,9 +57,9 @@ class CallbackModule(CallbackBase):
     junos_has_2RE = junos_dict['has_2RE']
     
     if junos_has_2RE:
-        self._display.display('Device has 2 Routing engines.\nPlease find details below:\n')
+        self._display.display('Device has 2 Routing engines. Please find details below:')
     else:
-        self._display.display('Device has only 1 Routing engine.\nPlease find details below:\n')
+        self._display.display('Device has only 1 Routing engine. Please find details below:')
     
     self._display.display('\tSERIAL NUMBER: {}'.format(junos_dict['serialnumber']))   
     self._display.display('\tMASTER RE: {}'.format(junos_dict['master']))
@@ -67,10 +67,10 @@ class CallbackModule(CallbackBase):
     self._display.display('\tSWITCH STYLE: {}'.format(junos_dict['switch_style']))
     
     if 'RE0' in junos_dict:
-        self._display.display('\n\tRouting Engine RE0:')
-        for key, val in junos_dict['RE0']:
+        self._display.display('\tRouting Engine RE0:')
+        for key, val in junos_dict['RE0'].items():
             self._display.display('\t\t{0}: {1}'.format(key.upper().replace('_', ' '), val))    
     if 'RE1' in junos_dict and junos_has_2RE:
-        self._display.display('\n\tRouting Engine RE1:')
-        for key, val in junos_dict['RE1']:
+        self._display.display('\tRouting Engine RE1:')
+        for key, val in junos_dict['RE1'].items():
             self._display.display('\t\t{0}: {1}'.format(key.upper().replace('_', ' '), val))
