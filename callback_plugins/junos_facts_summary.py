e1a901396e6101997c1ba0896d76391755ab1860
m83880 <jsin@tdc.dk>
2018-04-25 14:16:42 +0200

    Testing the facts plugin

--- callback_plugins/junos_facts_summary.py
+++ callback_plugins/junos_facts_summary.py
@@ -17,7 +17,7 @@ class CallbackModule(CallbackBase):
   """
   CALLBACK_VERSION = 2.0
   CALLBACK_TYPE = 'aggregate'
-  CALLBACK_NAME = 'show_junos_facts'
+  CALLBACK_NAME = 'junos_facts_summary'
 
 ## useful links regarding Callback
 ## https://github.com/ansible/ansible/blob/devel/lib/ansible/plugins/callback/__init__.py
@@ -48,18 +48,33 @@ class CallbackModule(CallbackBase):
     ## Check if dic return has all valid information
     #if module_name == '' or module_args == {}:                  # Commented because it is not coming in Juniper_junos_jsnapy module output
     #    return None
-    if 'action' not in module_args:
+    if 'ansible_facts' not in module_args:
         return None
-    # Extra check added so that it only runs for juniper_junos_jsnapy module only
-    if  module_args['action'] not in ('snapcheck', 'check') or not ('test_files' in module_args or 'config_file' in module_args):
-        return None
-    # Added the <''> because module name not coming in results in case of juniper_junos_jsnapy module. Only module args are available.
-    if module_name in ('juniper_junos_jsnapy', 'junos_jsnapy', '') and (module_args['action'] in ('snapcheck', 'check')):
+    ansible_facts = module_args['ansible_facts']
+    if 'junos' not in ansible_facts:
+        return
+    junos_dict = ansible_facts['junos']
 
-      ## Check if dict entry already exist for this host
-      host = result._host.name
-      if not host in self._results.keys():
-        self._results[host] = []
-
-      self._results[host].append(result)
-
+    if 'has_2RE' not in junos_dict:
+        return
+ 
+    junos_has_2RE = junos_dict['has_2RE']
+    
+    if junos_has_2RE:
+        self._display.display('Device has 2 Routing engines.\nPlease find details below:\n')
+    else:
+        self._display.display('Device has only 1 Routing engine.\nPlease find details below:\n')
+    
+    self._display.display('\tSERIAL NUMBER: {}'.format(junos_dict['serialnumber']))   
+    self._display.display('\tMASTER RE: {}'.format(junos_dict['master']))
+    self._display.display('\tModel: {}'.format(junos_dict['model']))
+    self._display.display('\tSWITCH STYLE: {}'.format(junos_dict['switch_style']))
+    
+    if 'RE0' in junos_dict:
+        self._display.display('\n\tRouting Engine RE0:')
+        for key, val in junos_dict['RE0']:
+            self._display.display('\t\t{0}: {1}'.format(key.upper().replace('_', ' '), val))    
+    if 'RE1' in junos_dict and junos_has_2RE:
+        self._display.display('\n\tRouting Engine RE1:')
+        for key, val in junos_dict['RE1']:
+            self._display.display('\t\t{0}: {1}'.format(key.upper().replace('_', ' '), val))
