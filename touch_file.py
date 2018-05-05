9549931a84f26fd2f82adc9e9ed47f87b882f2fe
m83880 <jsin@tdc.dk>
2018-05-05 14:47:09 +0200

    test

--- touch_file.py
+++ touch_file.py
@@ -1,6 +1,13 @@
 
+import paramiko
+
+transport = paramiko.Transport(("62.243.147.45", 22))
+transport.connect(username = "ansible", password = "ansible")
+sftp = paramiko.SFTPClient.from_transport(transport)
+f = sftp.open("/home/ansible/Logs/RouterSoftwareUpgrade/janme_test.log", "w")
+f.write("hello,world")
+f.close()
 
 
-
-with open('/tmp/Touch_test_file.log', 'w') as f:
-    f.write('Janmejay')
+#with open('/tmp/Touch_test_file.log', 'w') as f:
+#    f.write('Janmejay')
